3c25e42ba38829baaeb91b8ae3e0f7d0e13adb1f
Ido Feins <ifeins@gmail.com>
2015-03-03 16:26:41 +0200

    removed menu from contact details.

--- app/src/main/java/com/iic/shopingo/ui/ContactDetailsActivity.java
+++ app/src/main/java/com/iic/shopingo/ui/ContactDetailsActivity.java
@@ -3,8 +3,6 @@ package com.iic.shopingo.ui;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.EditText;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -33,28 +31,6 @@ public class ContactDetailsActivity extends ActionBarActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_contact_details);
     ButterKnife.inject(this);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_contact_details, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 
   @OnClick(R.id.contact_save_btn)
