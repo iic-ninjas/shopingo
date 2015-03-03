7855e0eb502620ab347ff5d37a571bf1773d4581
Ido Feins <ifeins@gmail.com>
2015-03-03 16:29:47 +0200

    removed unneeded menu from onboarding activity.

--- app/src/main/java/com/iic/shopingo/ui/OnboardingActivity.java
+++ app/src/main/java/com/iic/shopingo/ui/OnboardingActivity.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import butterknife.InjectView;
 import com.facebook.Session;
 import com.facebook.SessionState;
@@ -66,28 +64,6 @@ public class OnboardingActivity extends ActionBarActivity {
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
     facebookLifecycleHelper.onActivityResult(requestCode, resultCode, data);
-  }
-
-  @Override
-  public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.menu_onboarding, menu);
-    return true;
-  }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
   }
 
   private void onSessionStateChanged(Session session, SessionState sessionState, Exception e) {
