73852253711e829ac4d7b1a80221adae54dab6de
Ido Feins <ifeins@gmail.com>
2015-03-04 23:11:11 +0200

    added basic unit test support with junit4.

--- app/src/test/java/com/iic/shopingo/services/UserConnectorTest.java
+++ app/src/test/java/com/iic/shopingo/services/UserConnectorTest.java
@@ -1,26 +1,27 @@
 package com.iic.shopingo.services;
 
 import android.content.SharedPreferences;
-import android.test.AndroidTestCase;
+import org.junit.Before;
+import org.junit.Test;
 import org.mockito.Mockito;
 
 /**
  * Test for UserConnector
  * Created by ifeins on 3/4/15.
  */
-public class UserConnectorTest extends AndroidTestCase {
+public class UserConnectorTest {
 
   private UserConnector subject;
 
   private SharedPreferences sharedPreferences;
 
-  @Override
-  protected void setUp() throws Exception {
-    super.setUp();
+  @Before
+  public void setUp()  {
     sharedPreferences = Mockito.mock(SharedPreferences.class);
     subject = new UserConnector(sharedPreferences);
   }
 
+  @Test
   public void testConnectWithFacebook() {
     
   }
