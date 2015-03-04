443f9f25c647a17c1ec7f1c7d833478548d57bd1
Ido Feins <ifeins@gmail.com>
2015-03-04 16:03:11 +0200

    added initial test.

---
+++ app/src/androidTest/java/com/iic/shopingo/services/UserConnectorTest.java
@@ -0,0 +1,15 @@
+package com.iic.shopingo.services;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Test for UserConnector
+ * Created by ifeins on 3/4/15.
+ */
+public class UserConnectorTest extends AndroidTestCase {
+
+  public void testConnectWithFacebook() {
+
+  }
+
+}
