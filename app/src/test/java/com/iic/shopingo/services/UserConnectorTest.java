a2e8276b212f5ae966f06b253068ad692b19c4b5
Ido Feins <ifeins@gmail.com>
2015-03-05 01:19:23 +0200

    successful test with jmockit.

--- app/src/test/java/com/iic/shopingo/services/UserConnectorTest.java
+++ app/src/test/java/com/iic/shopingo/services/UserConnectorTest.java
@@ -1,9 +1,15 @@
 package com.iic.shopingo.services;
 
 import android.content.SharedPreferences;
+import bolts.Task;
+import com.facebook.FacebookRequestError;
+import com.facebook.Response;
+import com.iic.shopingo.dal.models.User;
+import mockit.Expectations;
+import mockit.Mocked;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mockito;
 
 /**
  * Test for UserConnector
@@ -13,17 +19,30 @@ public class UserConnectorTest {
 
   private UserConnector subject;
 
+  @Mocked
   private SharedPreferences sharedPreferences;
 
+  @Mocked
+  private Response fbResponse;
+
   @Before
-  public void setUp()  {
-    sharedPreferences = Mockito.mock(SharedPreferences.class);
+  public void beforeEach()  {
     subject = new UserConnector(sharedPreferences);
   }
 
+  /**
+   * It should return a faulted task
+   */
   @Test
-  public void testConnectWithFacebook() {
-    
+  public void testHandleFacebookFetchResultWhenLoginFails() {
+    new Expectations() {{
+      fbResponse.getError();
+      result = new FacebookRequestError(0, "no_internet", "No internet");
+    }};
+
+    Task<User>.TaskCompletionSource deferred = Task.create();
+    subject.handleFacebookFetchResult(null, fbResponse, deferred);
+    Assert.assertTrue("Task should be faulted", deferred.getTask().isFaulted());
   }
 
 }
