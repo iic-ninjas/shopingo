a0e3643d655a223b4a4ac153ab5f225c704573df
Ido Feins <ifeins@gmail.com>
2015-03-05 14:31:28 +0200

    initial working test without testing private method with jmockit.

--- app/src/test/java/com/iic/shopingo/services/UserConnectorTest.java
+++ app/src/test/java/com/iic/shopingo/services/UserConnectorTest.java
@@ -3,13 +3,16 @@ package com.iic.shopingo.services;
 import android.content.SharedPreferences;
 import bolts.Task;
 import com.facebook.FacebookRequestError;
+import com.facebook.Request;
+import com.facebook.RequestAsyncTask;
 import com.facebook.Response;
+import com.facebook.Session;
 import com.iic.shopingo.dal.models.User;
+import mockit.Delegate;
 import mockit.Expectations;
 import mockit.Mocked;
 import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
 
 /**
  * Test for UserConnector
@@ -25,24 +28,30 @@ public class UserConnectorTest {
   @Mocked
   private Response fbResponse;
 
+  @Mocked
+  private Session fbSession;
+
   @Before
-  public void beforeEach()  {
+  public void beforeEach() {
     subject = new UserConnector(sharedPreferences);
   }
 
-  /**
-   * It should return a faulted task
-   */
-  @Test
-  public void testHandleFacebookFetchResultWhenLoginFails() {
+  public void testConnectWithFacebookWhenLoginFails() {
     new Expectations() {{
       fbResponse.getError();
       result = new FacebookRequestError(0, "no_internet", "No internet");
+
+      Request.newMeRequest(fbSession, (Request.GraphUserCallback) any);
+      result = new Delegate<RequestAsyncTask>() {
+        RequestAsyncTask delegate(Session session, Request.GraphUserCallback callback) {
+          callback.onCompleted(null, fbResponse);
+          return null;
+        }
+      };
     }};
 
-    Task<User>.TaskCompletionSource deferred = Task.create();
-    subject.handleFacebookFetchResult(null, fbResponse, deferred);
-    Assert.assertTrue("Task should be faulted", deferred.getTask().isFaulted());
+    Task<User> task = subject.connectWithFacebook(fbSession);
+    Assert.assertTrue("task should be faulted", task.isFaulted());
   }
 
 }
