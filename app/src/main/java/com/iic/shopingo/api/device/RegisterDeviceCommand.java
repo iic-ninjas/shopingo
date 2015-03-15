d2a6c90591e3529391e6e4eedfa7017d3f021da6
Ido Feins <ifeins@gmail.com>
2015-03-15 17:40:06 +0200

    changed to new API.

--- app/src/main/java/com/iic/shopingo/api/device/RegisterDeviceCommand.java
+++ app/src/main/java/com/iic/shopingo/api/device/RegisterDeviceCommand.java
@@ -1,8 +1,9 @@
 package com.iic.shopingo.api.device;
 
 import com.iic.shopingo.api.ApiResult;
-import com.iic.shopingo.api.BaseApiRequest;
+import com.iic.shopingo.api.BaseApiCommand;
 import com.iic.shopingo.api.Constants;
+import com.iic.shopingo.api.Server;
 import com.iic.shopingo.api.models.ApiSimpleResponse;
 import java.io.IOException;
 import java.util.HashMap;
@@ -11,7 +12,7 @@ import java.util.Map;
 /**
  * Created by ifeins on 3/15/15.
  */
-public class RegisterDeviceCommand extends BaseApiRequest<ApiResult> {
+public class RegisterDeviceCommand extends BaseApiCommand<ApiResult> {
 
   private String gcmRegId;
 
@@ -26,7 +27,7 @@ public class RegisterDeviceCommand extends BaseApiRequest<ApiResult> {
       Map<String, Object> params = new HashMap<>();
       params.put(Constants.Parameters.DEVICES_REG_ID, this.gcmRegId);
 
-      ApiSimpleResponse response = server.post(Constants.Routes.REGISTER_DEVICE_PATH, ApiSimpleResponse.class, params);
+      ApiSimpleResponse response = Server.post(authToken, Constants.Routes.REGISTER_DEVICE_PATH, ApiSimpleResponse.class, params);
       return new ApiResult(response);
     } catch (IOException e) {
       return new ApiResult(e.getMessage());
